a40f4f6b8672cedcc77d4e3c109e29bfc808e844
dafub511 <131792163+dafub511@users.noreply.github.com>
2023-06-14 23:59:28 +0200

    Repositorio original Antonio

--- src/test/java/org/selenide/examples/cucumber/GoogleImageSearchStepDefinitions.java
+++ src/test/java/org/selenide/examples/cucumber/GoogleImageSearchStepDefinitions.java
@@ -4,31 +4,30 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
 
+import static com.codeborne.selenide.Selenide.screenshot;
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThanOrEqual;
 import static com.codeborne.selenide.Condition.disappear;
-import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
-import static com.codeborne.selenide.Selenide.screenshot;
 
 public class GoogleImageSearchStepDefinitions {
-    private String keyword;
+  private String keyword;
+  
+  @When("click \"Images\" link")
+  public void chooseImagesAsSearchTarget() {
+	$(byText("Images")).click();
+  }
 
-    @When("click \"Images\" link")
-    public void chooseImagesAsSearchTarget() {
-        $(byText("Images")).shouldBe(visible).click();
-    }
+  @When("enter a keyword {string} in input field")
+  public void enterKeyword(String keyword) {
+    this.keyword = keyword;
+    $(By.name("q")).val(keyword).pressEnter();
+	screenshot("Images");
+  }
 
-    @When("enter a keyword {string} in input field")
-    public void enterKeyword(String keyword) {
-        this.keyword = keyword;
-        $(by.name("q")).shouldBe(visible).val(keyword).pressEnter();
-        screenshot("Images");
-    }
-
-    @Then("at least top {int} matching images should be shown")
-    public void topTenMatchedImagesShouldBeShown(int resultsCount) {
-        $$(".rg_i").shouldHave(sizeGreaterThanOrEqual(resultsCount));
-    }
+  @Then("at least top {int} matching images should be shown")
+  public void topTenMatchedImagesShouldBeShown(int resultsCount) {
+    $$(".rg_i").shouldHave(sizeGreaterThanOrEqual(resultsCount));
+  }
 }
