31956da863289bb45fbc2cbeea4a5a51c8fa0773
dafub511 <131792163+dafub511@users.noreply.github.com>
2023-06-14 21:55:44 +0200

    Correciones

--- src/test/java/org/selenide/examples/cucumber/GoogleImageSearchStepDefinitions.java
+++ src/test/java/org/selenide/examples/cucumber/GoogleImageSearchStepDefinitions.java
@@ -7,27 +7,35 @@ import org.openqa.selenium.By;
 import static com.codeborne.selenide.Selenide.screenshot;
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThanOrEqual;
 import static com.codeborne.selenide.Condition.disappear;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
 public class GoogleImageSearchStepDefinitions {
-  private String keyword;
-  
-  @When("click \"Images\" link")
-  public void chooseImagesAsSearchTarget() {
-	$(byText("Images")).click();
-  }
+    private String keyword;
 
-  @When("enter a keyword {string} in input field")
-  public void enterKeyword(String keyword) {
-    this.keyword = keyword;
-    $(By.name("q")).val(keyword).pressEnter();
-	screenshot("Images");
-  }
+    @When("click \"Images\" link")
+    public void chooseImagesAsSearchTarget() {
+        $(byText("Accept All")).click();
+        $(byText("Accept All")).should(disappear);
 
-  @Then("at least top {int} matching images should be shown")
-  public void topTenMatchedImagesShouldBeShown(int resultsCount) {
-    $$(".rg_i").shouldHave(sizeGreaterThanOrEqual(resultsCount));
-  }
+        $(byText("Images")).shouldBe(visible);
+        $(byText("Images")).click();
+    }
+
+    @When("enter a keyword {string} in input field")
+    public void enterKeyword(String keyword) {
+        this.keyword = keyword;
+        $(byText("Accept All")).click();
+        $(byText("Accept All")).should(disappear);
+        $(by.name("q")).shouldBe(visible);
+        $(by.name("q")).val(keyword).pressEnter();
+        screenshot("Images");
+    }
+
+    @Then("at least top {int} matching images should be shown")
+    public void topTenMatchedImagesShouldBeShown(int resultsCount) {
+        $$(".rg_i").shouldHave(sizeGreaterThanOrEqual(resultsCount));
+    }
 }
